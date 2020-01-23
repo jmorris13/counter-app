4648b7be3ff4a36c49cdb721f4b737b1a5d6ff08
jmorris13 <jmorris1398@gmail.com>
2020-01-23 18:12:24 -0500

    changed spelling of 'NavBar' to 'Navigation Bar'

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -6,7 +6,7 @@ const NavBar = ({ totalCounters }) => {
   return (
     <nav className="navbar navbar-light bg-light">
       <a className="navbar-brand" href="#">
-        Navbar{" "}
+        Navigation Bar{" "}
         <span className="badge badge-pill badge-secondary">
           {totalCounters}
         </span>
